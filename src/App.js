25de4ea387b939c5dfcf447e7c0e8d40e0acc411
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-02-28 14:44:23 +0530

    changed branch to master

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>demo project</p>
+        <p>change branch to master </p>
         <a
           className="App-link"
           href="https://reactjs.org"
